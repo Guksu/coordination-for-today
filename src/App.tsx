3407aaed6139a450ccbcf63c6924ed04c8a6544d
Guksu <yung3152@daum.net>
2021-11-12 12:15:16 +0900

    세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { FC, useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./routes/Home";
 
-function App() {
-  return <>hi</>;
-}
+const App: FC = () => {
+  const [intro, setIntro] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setIntro(false);
+    }, 3000);
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 export default App;
---
+++ src/routes/Home.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const Home: FC = () => {
+  return <>오늘 뭐입 지</>;
+};
+
+export default Home;
